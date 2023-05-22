697f1f41c88cb3b9db1412f7719243b86c30f60a
Can Göktaş <goktascan1@gmail.com>
2023-05-22 15:11:52 +0300

    project created

---
+++ Source/VoxelWorld/VoxelWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoxelWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELWORLD_API AVoxelWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
